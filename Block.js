48d368c33fa66dfb816724970702467f82e406e8
Rockxy011106 <72133434+Rockxy011106@users.noreply.github.com>
2020-11-14 15:03:56 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+          restitution:0.0,
+            friction:0.01
+          
+        }
+        this.Visiblity = 255;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+     
+      
+      
+      display(){
+         //console.log(this.body.speed);
+   if(this.body.speed < 3){
+    var pos =this.body.position;
+    push()
+    rectMode(CENTER);
+    rect(pos.x, pos.y, this.width, this.height);
+    pop()
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+      if( this.Visiblity<0&&this.Visiblity>-105){
+        score++;
+      }
+     pop();
+   } 
+      }
+}
